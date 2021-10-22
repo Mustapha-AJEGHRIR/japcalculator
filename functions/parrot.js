cb8bfed8917ebd0d245524017dfba55bdd275d6f
Mustapha-AJEGHRIR <m5u9s00@gmail.com>
2021-10-22 16:05:28 +0200

    little modif on parrot

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,31 +1,40 @@
 exports.handler = async function(event, context) {
-  const requestBody = JSON.parse(event.body);
-  const message = requestBody.message;
+  
+  try{
 
-
-  console.log(event);
-
-  if (event.httpMethod === "OPTIONS") {
-    const headers = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers": "Content-Type",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-    };
+    const requestBody = JSON.parse(event.body);
+    const message = requestBody.message;
+    
+    console.log(event);
+    
+    if (event.httpMethod === "OPTIONS") {
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+      };
+      return {
+        statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
+        headers,
+        body: "This was a preflight call!",
+      };
+    }
+    
+    if (message) {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: message }),
+      };
+    }
     return {
-      statusCode: 200, // <-- Must be 200 otherwise pre-flight call fails
-      headers,
-      body: "This was a preflight call!",
+      statusCode: 400,
+      body: JSON.stringify({ message: "Nothing to repeat :(" }),
     };
-  }
-
-  if (message) {
+  }catch(e){
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: message }),
+      body: JSON.stringify({ message: "You should send a message in a JSON body" }),
     };
+
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Please write a message" }),
-  };
 };
